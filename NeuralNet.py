4af050774bd339c7f69e47153f2d79e9e9989b27
KevinHan1209 <kevinhan@utexas.edu>
2025-04-05 17:04:34 -0500

    performed deep learning

--- NeuralNet.py
+++ NeuralNet.py
@@ -3,14 +3,15 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class FeedForwardNN(nn.Module):
-    def __init__(self, input_size, hidden_sizes):
+    def __init__(self, input_size, hidden_sizes, dropout_prob=0.5):
         super(FeedForwardNN, self).__init__()
         layers = []
         in_size = input_size
 
-        # Dynamically create hidden layers
+        # Dynamically create hidden layers with dropout
         for hidden_size in hidden_sizes:
             layers.append(nn.Linear(in_size, hidden_size))
+            layers.append(nn.Dropout(dropout_prob))  # Add dropout after each hidden layer
             in_size = hidden_size
 
         # Add the output layer (cardinality of 3)
@@ -20,8 +21,10 @@ class FeedForwardNN(nn.Module):
         self.layers = nn.ModuleList(layers)
 
     def forward(self, x):
-        for layer in self.layers[:-1]:  # Apply activation for all but the last layer
-            x = F.relu(layer(x))
-        x = self.layers[-1](x)  # No activation for the output layer
+        for layer in self.layers[:-1]:  # Apply activation and dropout for all but the last layer
+            if isinstance(layer, nn.Linear):
+                x = F.relu(layer(x))
+            elif isinstance(layer, nn.Dropout):
+                x = layer(x)
+        x = self.layers[-1](x)  # No activation or dropout for the output layer
         return x
-
